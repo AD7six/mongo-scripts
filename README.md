881e20cbab451c4117ba34bc95a843de3d047307
AD7six <andydawson76@yahoo.co.uk>
2011-02-16 10:13:32 +0100

    adding template script

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Mongo scripts
 =========================
 
 Various scripts used for mongodb maintainance or data migrations
+
+Db scripts are run by:
+
+	mongo [args] dbname jsfile.js
---
+++ template.js
@@ -0,0 +1,35 @@
+/**
+ * Standard batch script
+ * Define the collection to run on, the general conditions, and in the middle of the loop what
+ * it's supposed to do
+ */
+
+var collection = 'collectionname';
+var conditions ={};
+
+var processed = 0;
+var step = 10;
+var total = db[collection].count(conditions);
+
+print('Found ' + total + ' ' + collection + ' to process');
+while (processed < total) {
+	var cCursor = db[collection].find( conditions ).sort( { _id : 1 } ).limit(step);
+	while (cCursor.hasNext()) {
+		Row = cCursor.next();
+
+        print( "updating " + Row._id);
+        db[collection].update(
+            { _id: Row._id },
+            { $set: {
+                }
+            }
+        );
+
+        conditions['_id'] = { $gt: Row._id };
+	}
+	processed += step;
+	print(processed + " " + collection + " processed");
+
+	conditions._id = {"$gt": Row._id};
+}
+print("finished");
