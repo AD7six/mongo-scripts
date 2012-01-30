dafa650aa7efe34f6521d5c6dd7342acc16f5777
AD7six <andydawson76@gmail.com>
2012-01-30 09:49:41 +0100

    add unused space

--- collectionSizes.js
+++ collectionSizes.js
@@ -7,10 +7,11 @@
 var i, size, count,
 	collections = db.getCollectionNames();
 	totalCount = 0,
-	totalSize = 0;
+	totalSize = 0,
+	totalUnused = 0;
 
 print(renderLine('wrap'));
-print(renderLine('header', db + " collections", "Count", "Size (Bytes)"));
+print(renderLine('header', db + " collections", "Count", "Unused (Bytes)", "Size (Bytes)"));
 print(renderLine('divider'));
 
 for(i in collections) {
@@ -19,16 +20,20 @@ for(i in collections) {
 	}
 	count = db[collections[i]].count();
 	size = db[collections[i]].totalSize();
+	unused = db[collections[i]].storageSize() - db[collections[i]].dataSize();
 
-	print(renderLine('data', collections[i], count, size));
+	print(renderLine('data', collections[i], count, unused, size));
 
 	totalCount += count;
 	totalSize += size;
+	totalUnused += unused;
 }
 
 print(renderLine('divider'));
-print(renderLine('data', 'Totals:', totalCount, totalSize));
+print(renderLine('data', 'Totals:', totalCount, totalUnused, totalSize));
 print(renderLine('wrap'));
+percent = Math.round(totalUnused * 100 / totalSize);
+print(percent + '% unused space');
 
 /**
  * A helper function to Render a line of the table
@@ -40,22 +45,22 @@ print(renderLine('wrap'));
  *
  * @return a string
  */
-function renderLine(type, one, two, three) {
+function renderLine(type, one, two, three, four) {
 	var line = '',
-		widths = [36, 14, 20],
-		padding = ['right', 'left', 'left'];
+		widths = [36, 14, 20, 20],
+		padding = ['right', 'left', 'left', 'left'];
 
 	if (type === 'divider') {
-		return renderLine('data', '', '', '').replace(/ /g, '-');
+		return renderLine('data', '', '', '', '').replace(/ /g, '-');
 	}
 	if (type === 'wrap') {
-		return renderLine('data', '', '', '').replace(/./g, '-');
+		return renderLine('data', '', '', '', '').replace(/./g, '-');
 	}
 
 	if (type === 'header') {
-		padding = ['center', 'center', 'center'];
+		padding = ['center', 'center', 'center', 'center'];
 	}
-	return '| ' + pad(one, widths[0], padding[0]) + ' | ' + pad(two, widths[1], padding[1]) + ' | ' + pad(three, widths[2], padding[2]) + ' |';
+	return '| ' + pad(one, widths[0], padding[0]) + ' | ' + pad(two, widths[1], padding[1]) + ' | ' + pad(three, widths[2], padding[2]) + ' | ' + pad(four, widths[3], padding[3]) + ' |';
 }
 
 /**
